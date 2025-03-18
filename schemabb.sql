e33efa09a1c146152469c8bb44cc5539e7a413a2
Skythii <tbo2marret@gmail.com>
2025-03-18 14:37:27 +0100

    Update schemabb.sql

--- schemabb.sql
+++ schemabb.sql
@@ -5,3 +5,12 @@ CREATE TABLE livres (
     titre TEXT NOT NULL,
     client TEXT NOT NULL,
 );
+
+DROP TABLE IF EXISTS clients;
+CREATE TABLE clients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    nom TEXT NOT NULL,
+    prenom TEXT NOT NULL,
+    adresse TEXT NOT NULL
+);
