b5563e8463bad9a8536c99aca0b269141e0978e5
Skythii <tbo2marret@gmail.com>
2025-03-18 14:34:48 +0100

    Update dbbibliotheque.py

--- dbbibliotheque.py
+++ dbbibliotheque.py
@@ -2,19 +2,19 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-with open('schema.sql') as f:
+with open('schemabb.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO livres (titre) VALUES (?)",('A lécole des sorciers'))
-cur.execute("INSERT INTO livres (titre) VALUES (?)",('La chambre des secrets'))
-cur.execute("INSERT INTO livres (titre) VALUES (?)",('Le prisonier d'ascaban'))
-cur.execute("INSERT INTO livres (titre) VALUES (?)",('La coupe de feu'))
-cur.execute("INSERT INTO livres (titre) VALUES (?)",('Lordre du phénix'))
-cur.execute("INSERT INTO livres (titre) VALUES (?)",('Le prince de sang mélé'))
-cur.execute("INSERT INTO livres (titre) VALUES (?)",('Les reliques de la mort'))
-cur.execute("INSERT INTO livres (titre) VALUES (?)",('Nan mais alo quoi'))
+cur.execute("INSERT INTO livres (titre, auteur) VALUES (?, ?)",('A lécole des sorciers', 'JK Rowling' ))
+cur.execute("INSERT INTO livres (titre) VALUES (?)",('La chambre des secrets', 'JK Rowling'))
+cur.execute("INSERT INTO livres (titre) VALUES (?)",('Le prisonier d'ascaban', 'JK Rowling'))
+cur.execute("INSERT INTO livres (titre) VALUES (?)",('La coupe de feu', 'JK Rowling'))
+cur.execute("INSERT INTO livres (titre) VALUES (?)",('Lordre du phénix', 'JK Rowling'))
+cur.execute("INSERT INTO livres (titre) VALUES (?)",('Le prince de sang mélé', 'JK Rowling'))
+cur.execute("INSERT INTO livres (titre) VALUES (?)",('Les reliques de la mort', 'JK Rowling'))
+cur.execute("INSERT INTO livres (titre) VALUES (?)",('Nan mais alo quoi', 'Thibault Demaret'))
 
 connection.commit()
 connection.close()
