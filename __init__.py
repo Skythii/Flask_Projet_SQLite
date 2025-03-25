53e5c485819d8e1444a8b63245263f89c7642602
Skythii <tbo2marret@gmail.com>
2025-03-25 10:17:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for, session #temp
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session 
 import sqlite3
 
 app = Flask(__name__)
